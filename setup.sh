88f2c0cde720e0194b68fcd15921126ac8099ae6
thinceller <kohei.ct@icloud.com>
2019-08-19 03:44:31 +0900

    Change .vimrc to .config/nvim/init.vim

--- setup.sh
+++ setup.sh
@@ -7,12 +7,11 @@ do
   [[ $file == ".git" ]] && continue
   [[ $file == ".DS_Store" ]] && continue
 
-  if [[ $file == ".vimrc" ]]; then
-    ln -sfnv $HOME/dotfiles/$file $HOME/$file
-    ln -sfnv $HOME/dotfiles/$file $HOME/.config/nvim/init.vim
-  else
-    ln -sfnv $HOME/dotfiles/$file $HOME/$file
-  fi
+  ln -sfnv $HOME/dotfiles/$file $HOME/$file
 done
 
+# init.vimのみ.vimrcとして$HOME直下に配置する
+ln -sfnv $HOME/dotfiles/.config/nvim/init.vim $HOME/.vimrc
+
 echo "Succeeded!"
+
