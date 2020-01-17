1e3b2e45e4ee4ffec25b13f3164d063f47c85c9a
thinceller <kohei.ct@icloud.com>
2020-01-18 04:36:40 +0900

    Add dependencies script

---
+++ bin/deps/golang.sh
@@ -0,0 +1,7 @@
+go get -u github.com/motemen/ghq
+go get -u golang.org/x/tools/cmd/goimports
+go get -u github.com/motemen/gore/cmd/gore
+go get -u github.com/mdempsky/gocodeo
+go get -u github.com/github/hub
+go get -u github.com/mattn/memo
+go get -u github.com/toshimaru/nyan
