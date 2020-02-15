880643dc7efa632fa5d44c600a458821e9ec175b
thinceller <kohei.ct@icloud.com>
2020-02-15 16:07:30 +0900

    Update

---
+++ bin/deps/brew.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+set -x
+
+brew install git
+brew install tig
+brew install htop
+brew install wget
+brew install gh
+brew install zsh
--- bin/deps/golang.sh
+++ bin/deps/golang.sh
@@ -1,7 +1,16 @@
-go get -u github.com/motemen/ghq
+#!/usr/bin/env sh
+
+set -x
+
+# golang 環境がない場合は exit
+if !(type go > /dev/null 2>&1); then
+  exit 1
+fi
+
+# インストール
+go get -u github.com/x-motemen/ghq
 go get -u golang.org/x/tools/cmd/goimports
 go get -u github.com/motemen/gore/cmd/gore
-go get -u github.com/mdempsky/gocodeo
 go get -u github.com/github/hub
 go get -u github.com/mattn/memo
 go get -u github.com/toshimaru/nyan
