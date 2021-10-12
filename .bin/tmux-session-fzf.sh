b6b79f6645a0b8de2aed4127e175d30cd4b01f0c
thinceller <thinceller@gmail.com>
2021-10-12 17:51:55 +0900

    Fix tmux shells

--- .bin/tmux-session-fzf.sh
+++ .bin/tmux-session-fzf.sh
@@ -8,5 +8,5 @@ envs="env TERM=$TERM"
 [[ -n "$FZF_DEFAULT_COMMAND" ]] && envs="$envs FZF_DEFAULT_COMMAND=$(printf %q "$FZF_DEFAULT_COMMAND")"
 
 if [ -n "$TMUX" ]; then
-  tmux popup -xC -yC -w$width -h$height -KER "$envs tmux list-sessions | fzf --height 100% | cut -d : -f 1 | xargs tmux switch -t"
+  tmux popup -xC -yC -w$width -h$height -E "$envs tmux list-sessions | fzf --height 100% | cut -d : -f 1 | xargs tmux switch -t"
 fi
