c6779f2bb263f17ed46cc5c225c1c582be0fece7
thinceller <thinceller@gmail.com>
2024-09-29 03:55:39 +0900

    update wezterm config

--- home-manager/programs/wezterm/default.nix
+++ home-manager/programs/wezterm/default.nix
@@ -3,9 +3,24 @@
     enable = true;
     package = wezterm-flake.packages.${pkgs.system}.default;
     extraConfig = ''
+      local mux = wezterm.mux
+      wezterm.on("gui-startup", function(cmd)
+        local tab, pane, window = mux.spawn_window(cmd or {})
+        window:gui_window():maximize()
+      end)
+
       return {
+        -- Font
         font = wezterm.font("HackGen Console NF"),
+        font_size = 16.0,
+
         use_ime = true,
+
+        -- Apperance
+        color_scheme = 'Night Owl (Gogh)',
+        window_background_opacity = 0.8,
+        macos_window_background_blur = 10,
+        window_decorations = "RESIZE",
       }
     '';
   };
