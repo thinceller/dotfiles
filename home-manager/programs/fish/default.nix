5bcbb44196431081df7ed22db44555a6bcd1a5f4
thinceller <thinceller@gmail.com>
2024-09-29 22:25:56 +0900

    improve shellAbbr of fish

--- home-manager/programs/fish/default.nix
+++ home-manager/programs/fish/default.nix
@@ -3,13 +3,22 @@
     enable = true;
     shellAbbrs = {
       gs = "git status --short --branch";
-      gcim = "git commit -m";
+      gcim = {
+        setCursor = true;
+        expansion = "git commit -m '%'";
+      };
       gcf = "git commit --fixup";
       gri = "git rebase -i";
-      gsw = "git switch -c";
+      gsw = {
+        setCursor = true;
+        expansion = "git switch -c '%'";
+      };
       gca = "git commit --amend --no-edit";
       gd = "git diff --no-index";
-      null = ">/dev/null 2>&1";
+      null = {
+        position = "anywhere";
+        expansion = ">/dev/null 2>&1";
+      };
     };
     plugins = [
       {
