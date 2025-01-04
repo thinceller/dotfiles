12ea8d2962fe16fb054835bca72194b44199c806
thinceller <thinceller@gmail.com>
2025-01-04 19:56:25 +0900

    add telescope

--- configs/.config/nvim/lua/plugins/init.lua
+++ configs/.config/nvim/lua/plugins/init.lua
@@ -7,13 +7,36 @@ require("jetpack.paq")({
     "haishanh/night-owl.vim",
     event = { "VimEnter", "ColorSchemePre" },
     config = function()
-      require("plugins.night-owl")
+      require("plugins/night-owl")
     end,
   },
   {
     "xiyaowong/nvim-transparent",
     config = function()
-      require("plugins.nvim-transparent")
+      require("plugins/nvim-transparent")
+    end,
+  },
+  -- lua plugins
+  "nvim-lua/plenary.nvim",
+  -- fuzzy finder
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    requires = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("plugins/telescope")
+    end,
+  },
+  {
+    "nvim-telescope/telescope-frecency.nvim",
+    config = function()
+      require("telescope").load_extension("frecency")
+    end,
+  },
+  {
+    "nvim-telescope/telescope-github.nvim",
+    config = function()
+      require("telescope").load_extension("gh")
     end,
   },
 })
