ba9b0cbd6e7c70b9ace5d2ee4973c7635ba5316a
thinceller <thinceller@gmail.com>
2025-01-05 00:43:15 +0900

    use packer.nvim style for vim-jetpack

--- configs/.config/nvim/lua/plugins/init.lua
+++ configs/.config/nvim/lua/plugins/init.lua
@@ -1,53 +1,53 @@
 vim.cmd("packadd vim-jetpack")
 
-require("jetpack.paq")({
-  { "tani/vim-jetpack" },
+require("jetpack.packer").startup(function(use)
+  use({ "tani/vim-jetpack" })
   -- color scheme
-  {
+  use({
     "oxfist/night-owl.nvim",
     config = function()
       require("plugins/night-owl")
     end,
-  },
-  {
+  })
+  use({
     "xiyaowong/nvim-transparent",
     config = function()
       require("plugins/nvim-transparent")
     end,
-  },
+  })
   -- lua plugins
-  "nvim-lua/plenary.nvim",
+  use("nvim-lua/plenary.nvim")
   -- denops
-  {
+  use({
     "vim-denops/denops.vim",
     requires = { "vim-denops/denops-shared-server.vim" },
     config = function()
       require("plugins/denops")
     end,
-  },
+  })
   -- fuzzy finder
-  {
+  use({
     "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
     requires = { "nvim-lua/plenary.nvim" },
     config = function()
       require("plugins/telescope")
     end,
-  },
-  {
+  })
+  use({
     "nvim-telescope/telescope-frecency.nvim",
     config = function()
       require("telescope").load_extension("frecency")
     end,
-  },
-  {
+  })
+  use({
     "nvim-telescope/telescope-github.nvim",
     config = function()
       require("telescope").load_extension("gh")
     end,
-  },
+  })
   -- file explorer
-  {
+  use({
     "stevearc/oil.nvim",
     requires = {
       "nvim-tree/nvim-web-devicons",
@@ -55,57 +55,57 @@ require("jetpack.paq")({
     config = function()
       require("plugins/oil")
     end,
-  },
+  })
   -- status line
-  {
+  use({
     "nvim-lualine/lualine.nvim",
     requires = { "oxfist/night-owl.nvim" },
     config = function()
       require("plugins/lualine")
     end,
-  },
+  })
   -- git
-  {
+  use({
     "lewis6991/gitsigns.nvim",
     config = function()
       require("plugins/gitsigns")
     end,
-  },
-  {
+  })
+  use({
     "kdheepak/lazygit.nvim",
     requires = { "nvim-lua/plenary.nvim" },
     config = function()
       require("plugins/lazygit")
     end,
-  },
+  })
   -- misc
-  "vim-jp/vimdoc-ja",
-  {
+  use("vim-jp/vimdoc-ja")
+  use({
     "famiu/bufdelete.nvim",
     config = function()
       require("plugins/bufdelete")
     end,
-  },
-  {
+  })
+  use({
     "shortcuts/no-neck-pain.nvim",
     config = function()
       require("plugins/no-neck-pain")
     end,
-  },
-  {
+  })
+  use({
     "windwp/nvim-autopairs",
     config = function()
       require("nvim-autopairs").setup()
     end,
-  },
-  {
+  })
+  use({
     "numToStr/Comment.nvim",
     config = function()
       require("Comment").setup()
     end,
-  },
-  "machakann/vim-sandwich",
-})
+  })
+  use("machakann/vim-sandwich")
+end)
 
 -- automatic plugin installation
 -- https://github.com/tani/vim-jetpack#automatic-plugin-installation-on-startup
