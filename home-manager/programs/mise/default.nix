0015d3b6d85ae17a1ffac55f4b9a0e6064b9d79b
thinceller <thinceller@gmail.com>
2025-03-23 00:07:07 +0900

    remove mise global tools

--- home-manager/programs/mise/default.nix
+++ home-manager/programs/mise/default.nix
@@ -3,15 +3,6 @@
   programs.mise = {
     enable = true;
     globalConfig = {
-      tools = {
-        node = "20";
-        deno = "latest";
-        go = "latest";
-        ruby = "3.3.0";
-        dotnet = "8.0.204";
-        usage = "latest";
-      };
-
       settings = {
         legacy_version_file = true;
       };
