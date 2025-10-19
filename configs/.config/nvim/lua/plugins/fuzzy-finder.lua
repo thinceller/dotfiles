553db00d7729ff73923df8eb692917eea63a648f
thinceller <thinceller@gmail.com>
2025-10-19 17:43:16 +0900

    update fuzzy-finder config

--- configs/.config/nvim/lua/plugins/fuzzy-finder.lua
+++ configs/.config/nvim/lua/plugins/fuzzy-finder.lua
@@ -4,6 +4,21 @@ return {
     event = "DeferredUIEnter",
     after = function()
       require("telescope").setup({
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+          live_grep = {
+            additional_args = function()
+              return { "--hidden" }
+            end,
+          },
+          grep_string = {
+            additional_args = function()
+              return { "--hidden" }
+            end,
+          },
+        },
         extensions = {
           frecency = {
             ignore_patterns = { "*.git/*", "*./tmp/*", "*/node_modules/*" },
@@ -13,18 +28,22 @@ return {
         },
       })
 
+      local builtin = require("telescope.builtin")
+      local frecency = require("telescope").extensions.frecency
+      local gh = require("telescope").extensions.gh
       local option = { noremap = true, silent = true }
 
       -- vim.keymap.set('n', '<Leader>ff', '<Cmd>Telescope find_files<CR>', option)
-      vim.keymap.set("n", "<Leader>ff", "<Cmd>Telescope git_files<CR>", option)
-      vim.keymap.set("n", "<Leader>f/", "<Cmd>Telescope live_grep<CR>", option)
-      vim.keymap.set("n", "<Leader>fb", "<Cmd>Telescope buffers<CR>", option)
-      vim.keymap.set("n", "<Leader>fc", "<Cmd>Telescope commands<CR>", option)
-      vim.keymap.set("n", "<Leader>fl", "<Cmd>Telescope current_buffer_fuzzy_find<CR>", option)
-      vim.keymap.set("n", "<Leader>gs", "<Cmd>Telescope git_status<CR>", option)
-      vim.keymap.set("n", "<Leader>ghp", "<Cmd>Telescope gh pull_request<CR>", option)
-      vim.keymap.set("n", "<Leader><Leader>", "<Cmd>Telescope frecency<CR>", option)
-      vim.keymap.set("n", "<C-g>", "<Cmd>Telescope grep_string<CR>", option)
+      vim.keymap.set("n", "<Leader>ff", builtin.git_files, option)
+      vim.keymap.set("n", "<Leader>f/", builtin.live_grep, option)
+      vim.keymap.set("n", "<Leader>fb", builtin.buffers, option)
+      vim.keymap.set("n", "<Leader>fc", builtin.commands, option)
+      vim.keymap.set("n", "<Leader>fk", builtin.keymaps, option)
+      vim.keymap.set("n", "<Leader>fl", builtin.current_buffer_fuzzy_find, option)
+      vim.keymap.set("n", "<Leader>gs", builtin.git_status, option)
+      vim.keymap.set("n", "<Leader>ghp", gh.pull_request, option)
+      vim.keymap.set("n", "<Leader><Leader>", frecency.frecency, option)
+      vim.keymap.set("n", "<C-g>", builtin.grep_string, option)
     end,
   },
 }
